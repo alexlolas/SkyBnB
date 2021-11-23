acaf2c1205e7acf00a2ad3641b6d93688fd1b9ad
alexlolas <alexanderlolas@yahoo.com>
2021-11-23 11:13:19 -0800

    Worked on Modals

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -12,14 +12,19 @@ class Navbar extends React.Component{
     if (!this.props.currentUser ) {
       return (
         <div className="header">
-          <img className="header-icon" src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"  />
+          <img className="header-icon" 
+          src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" 
+          />
+
           <div className="search-bar">
-          <input type="text" placeholder='Start your search' />
-            
-            <button  className="search-button">
-              <div className='search-icon'><SearchIcon /></div>
-              </button>
-             </div>
+            <input className="search-bar-input" type="text" placeholder='Start your search' />
+              <button  className="search-button">
+                  <div 
+                    className='search-icon'><SearchIcon />
+                  </div>
+             </button>
+          </div>
+
           <div className='header-right'>
             <div className='globe-icon'><GlobeIcon /></div>
             <Link to='/signup'>SignUp</Link> 
