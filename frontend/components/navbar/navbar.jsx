bd2284e6cbf5ea832dcf862a08fa3bf397188808
alexlolas <alexanderlolas@yahoo.com>
2021-11-22 17:34:31 -0800

    added some nav style

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -9,10 +9,14 @@ class Navbar extends React.Component{
   render(){
     if (!this.props.currentUser ) {
       return (
-        <div>
+        <div className="header">
           <img className="header-icon" src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"  />
-         <Link to='/signup'>SignUp</Link> 
-         <Link to='/login'>Login</Link> 
+          <input className="search-bar" type="text" />
+          <div className='header-right'>
+            <Link to='/signup'>SignUp</Link> 
+            <Link to='/login'>Login</Link> 
+
+          </div>
         </div>
       )
     } else {
