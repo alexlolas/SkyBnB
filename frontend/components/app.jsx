10c9b2eb2f5c81bdf70705c31c1b23293041d550
alexlolas <alexanderlolas@yahoo.com>
2021-11-23 11:35:04 -0800

    Put modal in login

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,11 +6,11 @@ import { Route } from "react-router-dom";
 import SignupContainer from './session/signup_container'
 import LoginContainer from './session/login_container'
 import NavbarContainer from './navbar/navbar_container'
+import Modal from "./modal/modal";
 const App = () => (
   <div>
-    {/* <SignupContainer />
-    <LoginContainer /> */}
     <NavbarContainer />
+    <Modal />
     <Route path='/signup' component={props => <SignupContainer {...props}/>}/>
     <Route path='/login' component={LoginContainer} />
   </div>
--- frontend/components/session/login_container.jsx
+++ frontend/components/session/login_container.jsx
@@ -1,6 +1,8 @@
 import { connect } from "react-redux";
 import { login, clearErrors } from "../../actions/session_actions";
 import LoginForm from "./login_form";
+import { openModal, closeModal } from "../../actions/modal_actions";
+import React from "react";
 
 const mSTP = ({errors}) => ({
   errors: errors.session
@@ -8,7 +10,13 @@ const mSTP = ({errors}) => ({
 
 const mDTp = dispatch => ({
   loginUser: user => dispatch(login(user)),
-  clearErrors: () => dispatch(clearErrors())
+  clearErrors: () => dispatch(clearErrors()),
+  closeModal: () => dispatch(closeModal()),
+  otherForm: (
+    <div className="session-button" onClick={() => dispatch(openModal('signup'))}>
+      Signup
+    </div>
+  )
 })
 
 export default connect(mSTP, mDTp)(LoginForm)
