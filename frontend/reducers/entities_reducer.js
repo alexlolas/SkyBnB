53118266ef1414f7e1d0489177b59558f8d29f3a
alexlolas <alexanderlolas@yahoo.com>
2021-11-28 22:22:55 -0800

    add review reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import usersReducer from "./users_reducer";
 import listingReducer from "./listings_reducer";
+import reviewReducer from "./reviews_reducer";
 import { combineReducers } from "redux";
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
-  listings: listingReducer
+  listings: listingReducer,
+  reviews: reviewReducer
 })
 
 export default entitiesReducer
